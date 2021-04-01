8d48c15fccaa2ad78ed279f85fc3d90256cde03e
Numair Dubas <numairdubas@gmail.com>
2021-04-01 23:42:44 +0000

    Made a simple and useless program even more simple and slightly less useless!

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # organ
-Alphabetically Organize
+*Alphabetically Organize*
 
-Organ will take a list inputted by the user, and arrange it in alphabetical order. It will write the finished list to a file named sort.txt.
+Organ will take an inputted list, and arrange it in alphabetical order
--- main.c
+++ main.c
@@ -1,33 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-    int index1, index2, total;
-    char temp[100];
-    char words[100][50];    // Fixed array used to store input
-    printf("Total amount of words (max 100): "); // Limits strings when getting input
-    scanf("%d", &total);
-    
-    for(index1 = 0; index1 < total; index1++) {
-        scanf("%s", words[index1]);
-    }
+int main(int argc, char *argv[])
+{
+    char buf[1024][100];  // [1 mib][1 kib]
+    int wc = -1;
+    do {
+        ++wc;
+    } while (fgets(buf[wc], 100, stdin) != NULL);
 
-    for(index1 = 0; index1 < total - 1; index1++) {
-        for(index2 = index1 + 1; index2 < total; index2++) {
-            if(strcmp(words[index1], words[index2]) > 0) { // First Run: strcmp(words[0], words[1])
-                strcpy(temp, words[index1]);    // Copy words[index1] to a temporary location 
-                strcpy(words[index1], words[index2]);   // Overwrite words[index1] with words[index2]
-                strcpy(words[index2], temp);    // Copy words[index1] to a temporary location 
+    char tmp[100];
+    for (int i = 0; i < wc - 1; ++i) {
+        for (int j = i + 1; j < wc; ++j) {
+            if (strncmp(buf[i], buf[j], 100) > 0) {
+                strncpy(tmp, buf[i], 100);
+                strncpy(buf[i], buf[j], 100);
+                strncpy(buf[j], tmp, 100);
             }
         }
     }
 
-    FILE * fptr = fopen("sort.txt", "w");
+    for (int i = 0; i < wc; ++i)
+        printf("%s", buf[i]);
 
-    fprintf(fptr, "Sorted List\n===============\n\n");
-    for(index1 = 0; index1 < total; index1++) {
-        fprintf(fptr, "%s\n", words[index1]);
-    }
-    fclose(fptr);
-    printf("\nList sorted in file sort.txt\n");
+    return 0;
 }
